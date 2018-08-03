c4cb6c7fb4311dd28f802f96c465167e0bb9bb5c
vasilstar <vasilstar97@gmail.com>
2018-08-03 16:10:41 +0500

    checkpoint -burger -service -button

---
+++ site/templates/layouts/layout_service.php
@@ -0,0 +1,17 @@
+<section class="header">
+  <a href="#" class="burger"></a>
+</section>
+<section class="service">
+  <div class="service__grid">
+    <div class="service__cell">
+      <div class="content">
+
+      </div>
+    </div>
+    <div class="service__cell">
+      <div class="background" style="background-image: url('<?=$config->urls->templates?>assets/img/service-2.png')">
+
+      </div>
+    </div>
+  </div>
+</section>
