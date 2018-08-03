6fc12a087ce136ddc251cb8fc9631d86445862c0
vasilstar <vasilstar97@gmail.com>
2018-08-03 16:45:12 +0500

    checkpoint -service

--- site/templates/layouts/layout_service.php
+++ site/templates/layouts/layout_service.php
@@ -5,13 +5,24 @@
   <div class="service__grid">
     <div class="service__cell">
       <div class="content">
-
+        <h2 class="content__heading">создание сайтов</h2>
+        <div class="content__text">
+          <p>Наши сайты загружаются намного быстрее аналогов
+(соответственно меньше отказов от сайта и выше конверсия)</p>
+          <p>реализуем любые задачи: от интерактивной карты выбора участка в коттеджном посселке, до создания 3D сайта вашего продукта
+</p>
+          <p>индивидуальная панель управления Вашим сайтом</p>
+          <p>оптимизация под все мобильные устройства не дополнительная опция, а сама собой разумеющаяся!</p>
+          <p>cайт нужен, чтобы он приносил заявки, а не для того, чтобы был</p>
+          <a href="" class="content__case">посмотреть кейсы</a>
+        </div>
+        <a href="" class="content__button button">рассчитать стоимость</a>
+        <ul class="content__items">
+          <li class="content__item"><a href="" class="content__link content__link_left">ведение соц. сетей</a></li>
+          <li class="content__item"><a href="" class="content__link content__link_right">контекстная реклама</a></li>
+        </ul>
       </div>
     </div>
-    <div class="service__cell">
-      <div class="background" style="background-image: url('<?=$config->urls->templates?>assets/img/service-2.png')">
-
-      </div>
-    </div>
+    <div class="service__cell background" style="background-image: url('<?=$config->urls->templates?>assets/img/service-2.png')"></div>
   </div>
 </section>
