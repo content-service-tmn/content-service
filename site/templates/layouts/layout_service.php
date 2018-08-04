9df21284d39a402c460813e5a872c8c36d0594d9
vasilstar <vasilstar97@gmail.com>
2018-08-04 13:55:22 +0500

    service desktop done

--- site/templates/layouts/layout_service.php
+++ site/templates/layouts/layout_service.php
@@ -3,26 +3,27 @@
 </section>
 <section class="service">
   <div class="service__grid">
-    <div class="service__cell">
+    <div class="service__cell service__cell_left">
       <div class="content">
         <h2 class="content__heading">создание сайтов</h2>
-        <div class="content__text">
-          <p>Наши сайты загружаются намного быстрее аналогов
-(соответственно меньше отказов от сайта и выше конверсия)</p>
-          <p>реализуем любые задачи: от интерактивной карты выбора участка в коттеджном посселке, до создания 3D сайта вашего продукта
-</p>
-          <p>индивидуальная панель управления Вашим сайтом</p>
-          <p>оптимизация под все мобильные устройства не дополнительная опция, а сама собой разумеющаяся!</p>
-          <p>cайт нужен, чтобы он приносил заявки, а не для того, чтобы был</p>
+          <ul class="content__text">
+            <li>Наши сайты загружаются намного быстрее аналогов
+            (соответственно меньше отказов от сайта и выше конверсия)</li>
+            <li>реализуем любые задачи: от интерактивной карты выбора участка в коттеджном посселке, до создания 3D сайта вашего продукта</li>
+            <li>индивидуальная панель управления Вашим сайтом</li>
+            <li>оптимизация под все мобильные устройства не дополнительная опция, а сама собой разумеющаяся!</li>
+          </ul>
+          <p><span class="content__quote">cайт нужен, чтобы он приносил заявки, а не для того, чтобы был</span></p>
           <a href="" class="content__case">посмотреть кейсы</a>
-        </div>
-        <a href="" class="content__button button">рассчитать стоимость</a>
+          <div class="content__button">
+            <a href="" class="button">рассчитать стоимость</a>
+          </div>
         <ul class="content__items">
           <li class="content__item"><a href="" class="content__link content__link_left">ведение соц. сетей</a></li>
           <li class="content__item"><a href="" class="content__link content__link_right">контекстная реклама</a></li>
         </ul>
       </div>
     </div>
-    <div class="service__cell background" style="background-image: url('<?=$config->urls->templates?>assets/img/service-2-b.png')"></div>
+    <div class="service__cell service__cell_right background" style="background-image: url('<?=$config->urls->templates?>assets/img/service-2-b.png')"></div>
   </div>
 </section>
