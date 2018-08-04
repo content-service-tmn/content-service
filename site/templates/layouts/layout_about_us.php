827132a523aa50c205e345ca6a27a39c9bd24f1d
vasilstar <vasilstar97@gmail.com>
2018-08-04 23:49:25 +0500

    contacts checkpoint

--- site/templates/layouts/layout_about_us.php
+++ site/templates/layouts/layout_about_us.php
@@ -1,1 +1,63 @@
-hello about us!
+<section class="header header_about header_fixed">
+  <a href="#" class="burger"></a>
+</section>
+<section class="about">
+  <div class="about__grid">
+    <div class="about__cell about__cell_left">
+      <div class="about__image" style="background-image:url('<?=$config->urls->templates?>assets/img/contacts.png')"></div>
+    </div>
+    <div class="about__cell about__cell_right">
+      <div class="content">
+        <div class="about__items">
+          <div class="about__item">
+            <h2 class="about__heading">Кто мы?</h2>
+            <div class="about__text">
+              <p>Content Service – digital агенство полного цикла.</p>
+              <p>Наша основная компетенция создание системы продаж в интернете.</p>
+              <p>
+                Иначе говоря, интернет-филиала Вашей компании.
+              </p>
+            </div>
+          </div>
+          <div class="about__item">
+            <h2 class="about__heading">Мы можем</h2>
+            <div class="about__text">
+              <p>
+                Создать стиль компании
+              </p>
+              <p>
+                Разработать сайт
+              </p>
+              <p>
+                Привести на него целевую аудиторию
+              </p>
+              <p>
+                Учесть и настроить отдел продаж
+              </p>
+              <p>
+                Вести корпоративные соц сети
+              </p>
+              <p>
+                Создавать в соц сетях максимальные охваты аудитории
+              </p>
+            </div>
+          </div>
+          <div class="about__item">
+            <h2 class="about__heading">мы гордимся</h2>
+            <div class="about__text">
+              <p>
+                24 сотрудника
+              </p>
+              <p>
+                12 дней среднее время создания сайта
+              </p>
+              <p>
+                45% максимальное увеличение продаж у нашего заказчика                 
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
