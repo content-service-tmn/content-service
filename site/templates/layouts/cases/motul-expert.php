9e38630aec60a67fe4de254df56f653e2d1b8183
vasilstar <vasilstar97@gmail.com>
2018-08-06 12:23:33 +0500

    checkpoint

--- site/templates/layouts/cases/motul-expert.php
+++ site/templates/layouts/cases/motul-expert.php
@@ -8,4 +8,49 @@
       <p>Мы создали корпоративный сайт согласно брендбуку компании. Добавили в него продуктовую линейку с ценами и сервис по подбору масла для конкретного автомобиля.</p>
     </div>
   </div>
+  <div class="case__grid">
+    <div class="case__cell case__cell_big">
+      <img src="<?=$config->urls->templates?>assets/img/motul-3.jpg" class="case__img" alt="">
+    </div>
+  </div>
+  <div class="case__grid">
+    <div class="case__cell case__cell_big">
+      <img src="<?=$config->urls->templates?>assets/img/motul-2.jpg" class="case__img" alt="">
+    </div>
+  </div>
+  <div class="case__grid">
+    <div class="case__cell adaptive-block">
+      <img src="<?=$config->urls->templates?>assets/img/motul-phone.png" class="adaptive-block__img" alt="">
+    </div>
+    <div class="case__cell case__cell_motul-light case__cell_text">
+      <h2>адаптивка</h2>
+    </div>
+  </div>
+  <div class="case__grid">
+    <div class="case__cell case__cell_motul-dark case__cell_text">
+      <h2>рекламная кампания</h2>
+      <p>Мы настраивали рекламные кампании в Яндекс, настраивали call tracking и в первый же месяц обнаружили слабые места мастера-приемщика. Которые в скором времени устранили и получили в среднем 30 заявок в месяц в июле 2018 года на 45% увеличили оборот в рублях по отношению к июлю 2017 года.</p>
+    </div>
+    <div class="case__cell case__cell_motul-light case__cell_text indicators">
+      <h2>показатели в числах</h2>
+      <div class="indicators__grid">
+        <div class="indicators__cell">
+          <h2 class="indicators__value">31</h2>
+          <p class="indicators__text">Количество заявок</p>
+        </div>
+        <div class="indicators__cell">
+          <h2 class="indicators__value">34%</h2>
+          <p class="indicators__text">Рост продаж в рублях</p>
+        </div>
+        <div class="indicators__cell">
+          <h2 class="indicators__value">34</h2>
+          <p class="indicators__text">Цена клика</p>
+        </div>
+        <div class="indicators__cell">
+          <h2 class="indicators__value">4.39%</h2>
+          <p class="indicators__text">Конверсия в заявку</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
