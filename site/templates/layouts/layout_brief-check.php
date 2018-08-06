181f97b85e6e2ab8cd9f9bb3d1c3920e1645d63c
nikon72ru <nikon72ru@gmail.com>
2018-08-06 15:54:37 +0500

    new brief rendering

--- site/templates/layouts/layout_brief-check.php
+++ site/templates/layouts/layout_brief-check.php
@@ -1,3 +1,6 @@
+<section id="current_quest" class="brief">
+</section>
+
 <?php
 function continueButtonContent($next)
 {
@@ -82,7 +85,6 @@ function buildMultiline($quest, $id)
 }
 
 
-
 function buildQuest($quest, &$names)
 {
 
@@ -96,10 +98,9 @@ function buildQuest($quest, &$names)
         }
     }
 
-    if (isset($names[$quest->name])) {
-        return $names[$quest->name];
+    if (!isset($names[$quest->name])) {
+        $names[$quest->name] = $id;
     }
-    $names[$quest->name] = $id;
 
     switch ($quest->type) {
         case "radios":
@@ -116,19 +117,22 @@ function buildQuest($quest, &$names)
 }
 
 $briefs = json_decode(file_get_contents($page->brief_data->filename));
+$brief_content = file_get_contents($page->brief_data->filename);
 $validation = json_decode(file_get_contents($page->brief_validator->filename), 1);
 
 //add validators to brief
-function addValidator(&$quest, $validation){
+function addValidator(&$quest, $validation)
+{
     if (isset($quest->variables)) {
-        foreach ($quest->variables as $variable){
-            $variable->validator = isset($validation[$variable->varname])?$validation[$variable->varname]:null;
-            if (isset($variable->next)){
+        foreach ($quest->variables as $variable) {
+            $variable->validator = isset($validation[$variable->varname]) ? $validation[$variable->varname] : null;
+            if (isset($variable->next)) {
                 addValidator($variable->next, $validation);
             }
         }
     }
 }
+
 addValidator($briefs, $validation);
 
 
@@ -143,119 +147,232 @@ $(\"#{$briefs->id}\").removeClass(\"brief__disabled\")
 ?>
 
 <script>
-    $(".form-radios").not("brief__disabled").on("submit", function (e) {
-        var prevSection = $(this).closest("section");
-        var answer = $("input:checked");
-        if (answer[0] != undefined) {
-            var answerId = answer[0]["dataset"]["next"];
-            answers[$(prevSection).find(".brief__title")[0]["innerHTML"]] = answer.next("label")[0]["innerText"];
-            console.log( answer.next("label")[0]["innerText"]);
-            $("#" + answerId).removeClass("brief__disabled");
-            $(prevSection).addClass("brief__disabled");
-            answer.prop('checked', false);
-        }
-        e.preventDefault();
-    });
+    //    $(".form-radios").not("brief__disabled").on("submit", function (e) {
+    //        var prevSection = $(this).closest("section");
+    //        var answer = $("input:checked");
+    //        if (answer[0] != undefined) {
+    //            var answerId = answer[0]["dataset"]["next"];
+    //            answers[$(prevSection).find(".brief__title")[0]["innerHTML"]] = answer.next("label")[0]["innerText"];
+    //            console.log( answer.next("label")[0]["innerText"]);
+    //            $("#" + answerId).removeClass("brief__disabled");
+    //            $(prevSection).addClass("brief__disabled");
+    //            answer.prop('checked', false);
+    //        }
+    //        e.preventDefault();
+    //    });
 
-    $(".submit").on("click", function (e) {
-        var prevSection = $(this).closest("section");
-        var innerAnswers = {};
-        var hasError = false;
-        $(prevSection).find(".brief-form__text").each(function () {
-            var regex = new RegExp($(this).attr("data-validator"));
-            if (!regex.test($(this).val())){
-                $(this).addClass("error");
-                hasError = true;
-            }
-            innerAnswers[$(this).attr("placeholder")] = $(this)[0].value;
-        });
-        if (hasError) {
-            return false;
-        }
-        answers[$(prevSection).find(".brief__title")[0]["innerHTML"]] = innerAnswers;
-        var answer_id = $(this).attr("data-next");
-        if (answer_id) {
-            $("#" + answer_id).removeClass("brief__disabled");
-            $(prevSection).addClass("brief__disabled");
-        } else {
-            $.ajax({
-                url: "<?=$pages->find("template=ajax_handler")->first()->url?>",
-                type: 'POST',
-                data: {answers :answers},
-                success: function (result) {
-                    console.log(result);
-                }
-
-            });
-        }
-        e.preventDefault();
-    })
+//    $(".submit").on("click", function (e) {
+//        var prevSection = $(this).closest("section");
+//        var innerAnswers = {};
+//        var hasError = false;
+//        $(prevSection).find(".brief-form__text").each(function () {
+//            var regex = new RegExp($(this).attr("data-validator"));
+//            if (!regex.test($(this).val())) {
+//                $(this).addClass("error");
+//                hasError = true;
+//            }
+//            innerAnswers[$(this).attr("placeholder")] = $(this)[0].value;
+//        });
+//        if (hasError) {
+//            return false;
+//        }
+//        answers[$(prevSection).find(".brief__title")[0]["innerHTML"]] = innerAnswers;
+//        var answer_id = $(this).attr("data-next");
+//        if (answer_id) {
+//            $("#" + answer_id).removeClass("brief__disabled");
+//            $(prevSection).addClass("brief__disabled");
+//        } else {
+//            $.ajax({
+//                url: "<?//=$pages->find("template=ajax_handler")->first()->url?>//",
+//                type: 'POST',
+//                data: {answers: answers},
+//                success: function (result) {
+//                    console.log(result);
+//                }
+//
+//            });
+//        }
+//        e.preventDefault();
+//    })
 </script>
-<!--<section class="brief">-->
-<!--    <div class="brief__main">-->
-<!--        <div class="burger">-->
-<!--            <a href="#" class="burger_btn">1</a>-->
-<!--        </div>-->
-<!--        <div class="brief__wrap brief__wrap_margin">-->
-<!--            <h3 class="brief__title">Чем мы можем быть вам полезны?</h3>-->
-<!--            <form class="form">-->
-<!--                <input type="text" name="text" class="brief-form__text" placeholder="НАЗВАНИЕ">-->
-<!--            </form>-->
-<!--            <div class="submit">Далее</div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div class="brief__bg" style="background-image: url('');"></div>-->
-<!--</section>-->
-<!---->
-<!---->
-<!--<section class="brief">-->
-<!--    <div class="brief__main">-->
-<!--        <div class="burger">-->
-<!--            <a href="#" class="burger_btn">1</a>-->
-<!--        </div>-->
-<!--        <div class="brief__wrap brief__wrap_margin">-->
-<!--            <h3 class="brief__title">Чем мы можем быть вам полезны?</h3>-->
-<!--            <form class="form">-->
-<!--                <textarea class="brief-form__text brief-form__text_multiline" rows="1"></textarea>-->
-<!--            </form>-->
-<!--            <div class="submit">Далее</div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div class="brief__bg" style="background-image: url('');"></div>-->
-<!--</section>-->
-<!---->
-<!--<section class="brief">-->
-<!--    <div class="brief__main">-->
-<!--        <div class="burger">-->
-<!--            <a href="#" class="burger_btn">1</a>-->
-<!--        </div>-->
-<!--        <div class="brief__wrap">-->
-<!--            <h4 class="brief__begin">Заполнить бриф</h4>-->
-<!--            <h3 class="brief__title">Чем мы можем быть вам полезны?</h3>-->
-<!--            <form class="form">-->
-<!--                <div class="brief-form__item">-->
-<!--                    <input type="radio" name="question" id="one">-->
-<!--                    <label for="one">создать систему продаж в интернете</label>-->
-<!--                </div>-->
-<!--                <div class="brief-form__item">-->
-<!--                    <input type="radio" name="question" id="two">-->
-<!--                    <label for="two">создать сайт</label>-->
-<!--                </div>-->
-<!--                <div class="brief-form__item">-->
-<!--                    <input type="radio" name="question" id="three">-->
-<!--                    <label for="three">настроить рекламу в интернете</label>-->
-<!--                </div>-->
-<!--                <div class="brief-form__item">-->
-<!--                    <input type="radio" name="question" id="four">-->
-<!--                    <label for="four">вести социальные сети</label>-->
-<!--                </div>-->
-<!--                <div class="brief-form__item">-->
-<!--                    <input type="radio" name="question" id="five">-->
-<!--                    <label for="five">разработать фирменный стиль</label>-->
-<!--                </div>-->
-<!--            </form>-->
-<!--            <div class="submit">Далее</div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</section>-->
-<!---->
+
+<section id="current_quest">
+</section>
+
+<section id="brief_text" class="brief">
+    <div class="brief__main">
+        <div class="burger">
+            <a href="#" class="burger_btn">1</a>
+        </div>
+        <div class="brief__wrap brief__wrap_margin">
+            <h3 class="brief__title">pattern_title</h3>
+            <form class="form">
+                <input type="text" name="text" class="brief-form__text" placeholder="pattern_text">
+            </form>
+            <div class="submit">Далее</div>
+        </div>
+    </div>
+    <div class="brief__bg" style="background-image: url('');"></div>
+</section>
+
+
+<section id="brief_multiply" class="brief">
+    <div class="brief__main">
+        <div class="burger">
+            <a href="#" class="burger_btn">1</a>
+        </div>
+        <div class="brief__wrap brief__wrap_margin">
+            <h3 class="brief__title">pattern_title</h3>
+            <form class="form">
+                <textarea class="brief-form__text brief-form__text_multiline" rows="1" placeholder="pattern_text"></textarea>
+            </form>
+            <div class="submit">Далее</div>
+        </div>
+    </div>
+    <div class="brief__bg" style="background-image: url('');"></div>
+</section>
+
+<section id="brief_radios" class="brief radios">
+    <div class="brief__main">
+        <div class="burger">
+            <a href="#" class="burger_btn">1</a>
+        </div>
+        <div class="brief__wrap">
+            <h3 class="brief__title">pattern_title</h3>
+            <form class="form-radios">
+                <div class="brief-form__item">
+                    <input type="radio" name="quest" data-name="pattern_label_title" id="pattern_id">
+                    <label for="one">patter_text</label>
+                </div>
+            </form>
+            <div class="submit submit-radios">Далее</div>
+        </div>
+    </div>
+</section>
+
+
+<script>
+    console.log(<?=$brief_content?>);
+    function buildRadios(node) {
+        var current = $("#current_quest");
+        current.html($("#brief_radios").html());
+
+        current.html(current.html().replace("pattern_title", node.title));
+        var item = current.find(".form-radios");
+        var item_html = item.html();
+        var variables = "";
+        var i = 0;
+        node.variables.forEach(function (item) {
+            variables += item_html.replace("patter_text", item.vartitle).replace("pattern_label_title", item.vartitle).replace("pattern_id", "item_"+i);
+            i++;
+        });
+        item.html(variables);
+        current.find(".submit").on("click", function (e) {
+            var answer = current.find("input:checked");
+            if (answer[0] != undefined) {
+                answers[node.title] = answer.attr("data-name");
+                node.variables.forEach(function (item) {
+                    if (item.vartitle == answer.attr("data-name")){
+                        buildNode(item.next);
+                    }
+                });
+            }
+        });
+    }
+
+    function buildText(node) {
+        var current = $("#current_quest");
+        current.html($("#brief_text").html());
+
+        current.html(current.html().replace("pattern_title", node.title));
+        var item = current.find("form");
+        var item_html = item.html();
+        var variables = "";
+        node.variables.forEach(function (item) {
+            variables += item_html.replace("pattern_text", item.vartitle);
+        });
+        item.html(variables);
+        $(".submit").on("click", function (e) {
+            var innerAnswers = {};
+            var hasError = false;
+            $(current).find(".brief-form__text").each(function () {
+                var regex = new RegExp($(this).attr("data-validator"));
+                if (!regex.test($(this).val())) {
+                    $(this).addClass("error");
+                    hasError = true;
+                }
+                innerAnswers[$(this).attr("placeholder")] = $(this)[0].value;
+            });
+            if (hasError) {
+                console.log("false");
+                return false;
+            }
+            answers[node.title] = innerAnswers;
+
+            if (node.variables[0].next) {
+                buildNode(node.variables[0].next);
+            } else {
+                console.log("ajax");
+//                $.ajax({
+//                    url: "<?//=$pages->find("template=ajax_handler")->first()->url?>//",
+//                    type: 'POST',
+//                    data: {answers: answers},
+//                    success: function (result) {
+//                        console.log(result);
+//                    }
+//
+//                });
+            }
+            e.preventDefault();
+        })
+
+    }
+
+    function buildMultiply(node) {
+        var current = $("#current_quest");
+        current.html($("#brief_multiply").html());
+
+        current.html(current.html().replace("pattern_title", node.title));
+        var item = current.find("form");
+        var item_html = item.html();
+        var variables = "";
+        node.variables.forEach(function (item) {
+            variables += item_html.replace("pattern_text", item.vartitle);
+        });
+        item.html(variables);
+        if (node.variables[0].next) {
+            buildNode(node.variables[0].next);
+        } else {
+            console.log("ajax");
+//                $.ajax({
+//                    url: "<?//=$pages->find("template=ajax_handler")->first()->url?>//",
+//                    type: 'POST',
+//                    data: {answers: answers},
+//                    success: function (result) {
+//                        console.log(result);
+//                    }
+//
+//                });
+        }
+    }
+
+    function buildNode(node) {
+        $("#current_quest").html("");
+        console.log(node.title);
+        switch (node.type) {
+            case "radios":
+                buildRadios(node);
+                break;
+            case "text":
+                buildText(node);
+                break;
+            case "textarea":
+                buildMultiply(node);
+                break;
+        }
+
+    }
+
+    buildNode(<?=$brief_content?>);
+
+</script>
