b0186f2894e5580d4d7bd0d0ca6b67dabaa58306
vasilstar <vasilstar97@gmail.com>
2018-08-07 14:18:16 +0500

    audi

--- site/templates/layouts/cases/audi.php
+++ site/templates/layouts/cases/audi.php
@@ -1,1 +1,64 @@
-hello audi!
+<section class="header header_fixed">
+  <a href="#sidebar" class="burger" data-uk-offcanvas="{mode:'slide'}"></a>
+</section>
+<section class="home">
+  <div class="home__grid">
+    <div class="home__cell home__cell_left">
+      <div class="home__fixed-cell">
+        <div class="content">
+            <h2 class="content__heading">кейсы</h2>
+            <h3 class="content__subheading">мы создали это</h3>
+            <p><span class="content__quote content__quote_case">тут такое вот дело</span></p>
+            <p class="content__description">(Реклама в поисковых системах Яндекс и Google. Собираем только «горячие»
+                запросы той аудитории, кто готов покупать, а не тех, кто только мониторит рынок. Наша задача
+                привлечь максимальное количество целевого трафика по минимальной цене.)</p>
+        </div>
+      </div>
+    </div>
+    <div class="home__cell home__cell_right">
+      <section class="case">
+        <div class="case__grid">
+          <div class="case__cell case__cell_big">
+            <img src="<?=$config->urls->templates?>assets/img/audi-1.jpg" class="case__img" alt="">
+          </div>
+        </div>
+        <div class="case__grid">
+          <div class="case__cell case__cell_motul-dark case__cell_text">
+            <h2>audi tyumen</h2>
+            <h3>Ведение соц. сетей</h3>
+            <p>Один из самых ответственных кейсов по ведению социальных сетей. Все материалы согласовываются с Audi Rus. todo </p>
+          </div>
+          <div class="case__cell case__cell_motul-light case__cell_text indicators">
+            <h2>показатели в числах</h2>
+            <div class="indicators__grid">
+              <div class="indicators__cell">
+                <h2 class="indicators__value">31</h2>
+                <p class="indicators__text">Количество заявок</p>
+              </div>
+              <div class="indicators__cell">
+                <h2 class="indicators__value">34%</h2>
+                <p class="indicators__text">Рост продаж в рублях</p>
+              </div>
+              <div class="indicators__cell">
+                <h2 class="indicators__value">34</h2>
+                <p class="indicators__text">Цена клика</p>
+              </div>
+              <div class="indicators__cell">
+                <h2 class="indicators__value">4.39%</h2>
+                <p class="indicators__text">Конверсия в заявку</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="case__grid">
+          <div class="case__cell case__cell_big">
+            <img src="<?=$config->urls->templates?>assets/img/audi-2.jpg" class="case__img" alt="">
+          </div>
+          <div class="case__cell case__cell_big">
+            <img src="<?=$config->urls->templates?>assets/img/audi-3.jpg" class="case__img" alt="">
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</section>
