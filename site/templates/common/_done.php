46c77fc2dad37f42989bc5b66b127cde3f69bbba
vasilstar <vasilstar97@gmail.com>
2018-08-07 23:46:06 +0500

    check

--- site/templates/common/_done.php
+++ site/templates/common/_done.php
@@ -50,78 +50,33 @@
   </div>
 </div>
 
-<div class="layout">
-  <div class="layout__left-bar">
-    <div class="hello">
-      <h1>Hello</h1>
-      <h2>ContentService</h2>
-      <p>Создаем продажи в интернете</p>
-    </div>
-  </div>
-  <div class="layout__right-bar">
-    <div class="b-services">
-      <div class="b-services__grid-wrap">
-        <div class="b-services__grid">
-          <div class="b-services__cell">
-            <div class="b-service">
-              <img src="" alt=""/>
-              <a href=""></a>
-              <div class="b-service__overlay">
-                <p class="b-service__title">Создание контента</p>
-              </div>
+<div id="callback" class="uk-offcanvas">
+  <div class="uk-offcanvas-bar">
+    <a class="uk-offcanvas-close"></a>
+    <div class="uk-offcanvas-bar-inner sidebar">
+      <div class="callback sidebar__callback">
+        <form id="callback__form1" class="callback__form form" action="/" method="post">
+          <h2 class="callback__heading">оставить заявку</h2>
+          <div class="form__element form__element_name">
+            <label class="label" for="name1">Имя</label>
+            <input id="name1" class="input input_outline js-input" type="text" name="name" value="">
+          </div>
+          <div class="form__element form__element_phone">
+            <label class="label" for="phone1">Телефон</label>
+            <input id="phone1" class="input input_outline js-input js-phone" type="text" name="phone" value="">
+          </div>
+          <div class="form__element form__element_submit">
+            <div class="send">
+              <span class="send__text">отправить</span>
+              <button id="submit_form1" class="send__button" type="submit" name="submit" value="order"></button>
             </div>
           </div>
-          <div class="b-services__cell">
-            <div class="b-service">
-              <img src="" alt=""/>
-              <a href=""></a>
-              <div class="b-service__overlay">
-                <p class="b-service__title">Брендирование</p>
-              </div>
-            </div>
-          </div>
-          <div class="b-services__cell">
-            <div class="b-service">
-              <img src="" alt=""/>
-              <a href=""></a>
-              <div class="b-service__overlay">
-                <p class="b-service__title">Разработка сайта</p>
-              </div>
-            </div>
-          </div>
-          <div class="b-services__cell">
-            <div class="b-service">
-              <img src="" alt=""/>
-              <a href=""></a>
-              <div class="b-service__overlay">
-                <p class="b-service__title">Реклама</p>
-              </div>
-            </div>
-          </div>
-          <div class="b-services__cell">
-            <div class="b-service">
-              <img src="" alt=""/>
-              <a href=""></a>
-              <div class="b-service__overlay">
-                <p class="b-service__title">Реклама</p>
-              </div>
-            </div>
-          </div>
-          <div class="b-services__cell">
-            <div class="b-service">
-              <img src="" alt=""/>
-              <a href=""></a>
-              <div class="b-service__overlay">
-                <p class="b-service__title">Реклама</p>
-              </div>
-            </div>
-          </div>
-        </div>
+          <p class="form__text">Нажимая кнопку отправить, вы соглашаетесь на <a href="" target="_blank">обработку персональных данных</a></p>
+        </form>
       </div>
     </div>
   </div>
 </div>
-
 
 <?php endif; ?>
 
