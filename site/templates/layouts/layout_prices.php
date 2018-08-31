0e287ad66343fb2f49b24acb99d2e88e4f16ce4c
nikon72ru <nikon72ru@gmail.com>
2018-08-31 11:10:39 +0500

    new button

--- site/templates/layouts/layout_prices.php
+++ site/templates/layouts/layout_prices.php
@@ -18,7 +18,7 @@
             <div class="prices-block__text">
               <?=$price->price_content?>
             </div>
-            <a href="" class="prices-block__button">заказать</a>
+            <a href="#callback" id="leave_callback" data-from="<?=$price->price_header?>" data-uk-offcanvas="{mode:'slide'}" class="prices-block__button">заказать</a>
           </div>
         </div>
         <?php endforeach; ?>
